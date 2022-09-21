0303f96fc3d9749ffcc885c19a61fcc3f95235a1
Knut Kirkhorn <knutkirk@hotmail.com>
2022-09-21 16:14:27 +0200

    Change indentation from spaces to tabs

--- test.js
+++ test.js
@@ -3,30 +3,30 @@ import test from 'ava';
 import weekNumber from './index.js';
 
 test('current date returns a number', t => {
-    const expectedResult = 'number';
-    const result = typeof weekNumber();
+	const expectedResult = 'number';
+	const result = typeof weekNumber();
 
-    t.is(result, expectedResult);
+	t.is(result, expectedResult);
 });
 
 test('new date returns a number', t => {
-    const expectedResult = 'number';
-    const result = typeof weekNumber(new Date());
+	const expectedResult = 'number';
+	const result = typeof weekNumber(new Date());
 
-    t.is(result, expectedResult);
+	t.is(result, expectedResult);
 });
 
 test('input string date returns a number', t => {
-    const expectedResult = 'number';
-    const result = typeof weekNumber('September 3, 2019 13:33:37');
+	const expectedResult = 'number';
+	const result = typeof weekNumber('September 3, 2019 13:33:37');
 
-    t.is(result, expectedResult);
+	t.is(result, expectedResult);
 });
 
 test('invalid input throws error', t => {
-    t.throws(() => weekNumber(1337));
+	t.throws(() => weekNumber(1337));
 });
 
 test('invalid input string throws error', t => {
-    t.throws(() => weekNumber('invalid date :)'));
+	t.throws(() => weekNumber('invalid date :)'));
 });
